cb507cc21eb5bc266b95d5c93c1381a730db982e
yuu-eguci <yuu.eguci@gmail.com>
2021-08-03 12:50:35 +0900

    🔊 ChatHub.cs でログを出力する

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,13 +1,22 @@
 using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace SignalRChat.Hubs
 {
     public class ChatHub : Hub
     {
+        private readonly ILogger<ChatHub> _logger;
+
+        public ChatHub(ILogger<ChatHub> logger)
+        {
+            this._logger = logger;
+        }
+
         // 接続されたクライアントから呼び出されます。
         public async Task SendMessage(string user, string message)
         {
+            _logger.LogInformation($"ChatHub.SendMessage からのログ!!!! 飛んできた引数は…… {user}: {message}");
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
     }
